8d0711334730e68cf2eff6a8183318c87797869d
Matthew Rowe <09mrowe@gmail.com>
2026-08-22 21:35:06 +0100

    Initial code upload

---
+++ m_sequences.py
@@ -0,0 +1,58 @@
+""" The main script to calculate the offset of a product of M-sequences. This is
+equivalently calculating the Zech logarithms of the Galois field GF(2**n). """
+
+import numpy as np
+from scipy.ndimage import correlate1d
+from scipy.signal import max_len_seq
+
+class MSequence:
+    def __init__(self, MSeq: np.ndarray):
+        self.MSeq = MSeq
+        self.length = len(MSeq)
+        self.base_peak = self._find_peak(correlate1d(MSeq, MSeq, mode='wrap'))
+
+    def find_shift(self, shifted_array: np.ndarray):
+        derived_impulse = correlate1d(self.MSeq, shifted_array, mode='wrap')
+        derived_peak = self._find_peak(derived_impulse)
+
+        return (self.base_peak - derived_peak)%self.length
+
+    def find_product_offsets(self):
+        self.product_offsets = np.array([self.find_shift(self.MSeq*self._shift(self.MSeq, i)) 
+                                        for i in range(1, self.length)])
+
+    @staticmethod
+    def _shift(arr: np.ndarray, shift: int):
+        return np.roll(arr, shift)
+
+    @staticmethod
+    def _find_peak(impulse: np.ndarray):
+        return np.argmax(abs(impulse))
+
+
+class MSequenceFromFile(MSequence):
+    def __init__(self, path: str):
+        super().__init__(self.load(path))
+
+    def load(self, path: str):
+        """ Ensure M-sequence is {-1,1} not {0,1}. Note for nbits >= 32 there will
+        be an overflow error due to the int32. """
+        file_data = np.loadtxt(path, dtype=np.int32)
+
+        return 2*file_data - 1 if 0 in np.unique(file_data) else file_data
+
+
+class MSequenceFromScipy(MSequence):
+    def __init__(self, order: int, state: str, taps: str):
+        super().__init__(self.load(order, state, taps))
+
+    def load(self, order: int, state: str, taps: str):
+        """ Gets scipy to generate an M-sequence with the provided arguments. """
+        state = list(map(int, state.split(','))) if state is not None else None
+        taps = list(map(int, taps.split(','))) if taps is not None else None
+
+        m = 2*max_len_seq(order, state=state, taps=taps)[0]-1
+
+        return m.astype(np.int32, copy=False)
+
+# Is typing correct
---
+++ main.py
@@ -0,0 +1,41 @@
+import argparse
+from products import MSequenceFromFile, MSequenceFromScipy
+
+# ORDER_8_PATH = "DATA/Random/Compensation/m_sequence_8.txt"
+# ORDER_6_PATH = "DATA/Random/Compensation/m_sequence_6.txt"
+
+def main():
+    """ Entry point for the program """
+    args = parse_command_line_input()
+
+    if args.file_path is not None:
+        m_seq = MSequenceFromFile(args.file_path)
+    elif args.nbits is None:
+        raise Exception('Either a file or an integer must be provided as arguments.')
+    else:
+        m_seq = MSequenceFromScipy(args.nbits, args.state, args.taps)
+
+    m_seq.find_product_offsets()
+    print(m_seq.product_offsets)
+
+def parse_command_line_input():
+    """ Parse the input. Either a text file containing the M-sequence must be provided,
+     or inputs to interface with scipy.signal.max_len_seq(). """
+
+    parser = argparse.ArgumentParser(argument_default=None)
+    parser.add_argument('file_path', nargs='?', type=str, 
+                        help='Path of text file containing the M-sequence (optional)')
+    parser.add_argument('-n', '--nbits', type=int, 
+                        help='The order of the M-sequence (required if no path provided, overridden otherwise)')
+    parser.add_argument('-s', '--state', type=str,
+                        help='Initial state of the shift register')
+    parser.add_argument('-t', '--taps', type=str,
+                        help='Primitive polynomial coefficients (feedback taps)')
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
+
+    # Can the n argument be positional as well?
