d707c0bb0b83e81f87328e46defc74490f78f0d5
Matthew Rowe <09mrowe@gmail.com>
2026-08-29 21:29:33 +0100

    Representatives for gold codes

--- decimations.py
+++ decimations.py
@@ -1,3 +1,7 @@
+""" Decimating (sub-sampling) an m-sequence returns another m-sequence. Only integers coprime 
+to the sequence length, L =  2^n - 1, will form an interesting decimation. These phi(2^n-1) 
+decimations give a distinct m-sequence rather than a circular shift of the original. """
+
 from main import parse_command_line_input, validate_input
 import numpy as np
 from scipy.ndimage import correlate1d
@@ -11,41 +15,52 @@ class Decimations:
         self.find_gold_decimations()
 
     def find_non_trivial_decimations(self):
-        """ Only integers coprime to the sequence length, 2^n - 1, will form an interesting decimation.
-        These phi(2^n-1) decimations give a distinct m-sequence rather than a circular shift of the original. 
-        
-        However as there are only phi(2^n-1)/n unique m-sequences at a given order, we further remove this 
-        multiplicity (powers of two of a primitive element is also primitive with the same polynomial). """
+        """ Finds coprime decimations. As there are only phi(2^n-1)/n unique m-sequences at a 
+        given order, we further remove the multiplicity arising from powers of two. That is,
+        only one representative from the cycle i*{1,2,4, ..., 2^{n-1}} is required; the square 
+        of a primitive element is also primitive with the same polynomial. """
 
         candidate_integers = np.arange(1, self.L, dtype=int)
 
         gcd = np.gcd(candidate_integers, self.L)
         coprime_integers = candidate_integers[gcd == 1]
 
-        primitive_element_cycles = [ [i*(2**k)%self.L for k in range(self.n)] for i in coprime_integers]
+        primitive_element_cycles = [self._cycle(i) for i in coprime_integers]
         representatives = set([min(cycle) for cycle in primitive_element_cycles])
 
         self.non_trivial_decimations = sorted(list(representatives))
         self.num_primitive_polynomials = len(self.non_trivial_decimations)
 
     def find_gold_decimations(self):
-        candidate_integers = np.arange(1, self.n, dtype=int)
-        gold_exponents = [i for i in candidate_integers if (self.n/np.gcd(self.n,i))%2 == 1]
+        """ Particular decimations that give nice three-valued cross-correlations with the 
+        original sequence. """
 
-        self.gold_decimations = [2**k + 1 for k in gold_exponents]
+        candidate_integers = np.arange(1, self.n, dtype=int)
+        all_gold_decimations = [2**i + 1 for i in candidate_integers if (self.n/np.gcd(self.n,i))%2 == 1]
+
+        gold_representatives = set([min(self._cycle(i)) for i in all_gold_decimations])
+
+        self.gold_decimations = sorted(list(gold_representatives))
+
+    def _cycle(self, i: int):
+        return [i*(2**k)%self.L for k in range(self.n)]
 
     def summarise(self):
         print("*"*80, "SUMMARY", sep="\n")
-        print(f"There are {self.num_primitive_polynomials} unique m-sequences at n = {self.n}, produced by decimating the original at:")
+        print(f"There are {self.num_primitive_polynomials} unique m-sequences at n = {self.n}, ",
+              "produced by decimating the original at:", sep="")
         print("d = ", self.non_trivial_decimations)
         print(f"Of these {self.gold_decimations} are Gold decimations.\n" + "*"*80)
 
 
 def sanity_check(MSeq: np.ndarray):
+    """ M-sequences have two-level auto-correlations. """
+
     auto_correlation = correlate1d(MSeq, MSeq, mode='wrap')
     unique_levels = np.unique(auto_correlation).size
 
     return unique_levels == 2
+
 
 if __name__ == "__main__":
 
@@ -55,8 +70,8 @@ if __name__ == "__main__":
     decimations = Decimations(m_seq.length)
 
     for d in decimations.non_trivial_decimations:
-        print(f"DECIMATION: d = {d}")
-        print(f"GOLD TYPE: {d in decimations.gold_decimations}")
+        print(f"DECIMATION: d = {d}", 
+              f"GOLD TYPE: {d in decimations.gold_decimations}", sep="\n")
 
         new_m_sequence = m_seq.decimate(d)
         if not sanity_check(new_m_sequence):
@@ -67,8 +82,3 @@ if __name__ == "__main__":
         print(f"{unique_levels.size} LEVEL cross-correlation with d = 1: ", unique_levels, "\n")
 
     decimations.summarise()
-
-
-    # TODO Sort out docstrings and typing
-    # Gold sequences are not working!
-    # Bitbucket change gold codes to gold decimations
